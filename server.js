573a96501d82a9dfd4d94bc57524c6fc739ac034
pankajmakear <pankaj.p@makear.ai>
2025-03-12 18:34:24 +0530

    add vercel

--- server.js
+++ server.js
@@ -1,25 +1,17 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 
-// Create an Express app
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-// Middleware to parse JSON and URL encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Set the view engine to EJS and specify the views folder
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 
-// Define the route to render the index.ejs file
-app.get('/', (req, res) => {
-  res.render('index'); // Renders index.ejs in the views folder
+app.get("/", (req, res) => {
+  res.render("index"); // Renders index.ejs in the views folder
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
